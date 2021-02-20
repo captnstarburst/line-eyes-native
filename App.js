8a26db1cf887820168418adf52e760fb0ffa24a7
captnstarburst <captnstarburst@gmail.com>
2021-02-20 16:11:43 -0500

    added ignore for useNativeDriver warning, no fix

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NativeRouter, Route} from 'react-router-native';
 // import ROUTES from './components/constants/routes';
 import LogInPage from './components/LogInPage';
@@ -11,7 +11,14 @@ import AdminPage from './components/AdminPage';
 import Firebase, {withFirebase} from './components/Firebase';
 import {withAuthentication} from './components/Session';
 
+import {LogBox} from 'react-native';
+
 const App = (props) => {
+  useEffect(() => {
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+    // https://stackoverflow.com/questions/61014661/animated-usenativedriver-was-not-specified-issue-of-reactnativebase-input
+  }, []);
+
   return (
     <NativeRouter>
       <Route exact path={'/'} component={Landing} />
